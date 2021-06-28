83feee5bcbe44715c7095a955caa6bf65c1fd1a1
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-06-28 20:54:14 +0530

    Update mongo.py

--- GabiBraunRobot/mongo.py
+++ GabiBraunRobot/mongo.py
@@ -6,7 +6,7 @@ from motor import motor_asyncio
 from GabiBraunRobot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from AsunaRobot.conf import get_int_key, get_str_key
+from GabiBraunRobot.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")

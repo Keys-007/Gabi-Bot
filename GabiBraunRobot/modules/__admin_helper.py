19e240e4a67288470a339cad704eb90ac8ee11f6
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-06-29 22:44:14 +0530

    Update __admin_helper.py

--- GabiBraunRobot/modules/__admin_helper.py
+++ GabiBraunRobot/modules/__admin_helper.py
@@ -1,37 +1,63 @@
-import asyncio
-from datetime import datetime
-from telethon.tl.functions.channels import EditAdminRequest
-from telethon.tl.types import ChatAdminRights
-from telethon import events
-from GabiBraunRobot.events import register as gabi
+
+import html
+
+from pyrogram.utils.exceptions import ChatAdminRequired
+from telethon.errors import AdminRankEmojiNotAllowedError
+
+from GabiBraunRobot import BOT_ID, bot
+from GabiBraunRobot.telethon import register
+from GabiBraunRobot.services.telethon import tbot
+
+from .utils.connections import chat_connection
+from .utils.language import get_strings_dec
+from .utils.user_details import (
+    get_admins_rights,
+    get_user_and_text_dec,
+    get_user_dec,
+    get_user_link,
+)
 
 
-@gabi(pattern="^/fullpromote ?(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    start = datetime.now()
-    to_promote_id = None
-    rights = ChatAdminRights(
-        change_info=True,
-        post_messages=True,
-        edit_messages=True,
-        delete_messages=True,
-        ban_users=True,
-        invite_users=True,
-        pin_messages=True,
-        add_admins=True,
+@register(cmds="promote", bot_can_promote_members=True, user_can_promote_members=True)
+@chat_connection(admin=True, only_groups=True)
+@get_user_and_text_dec()
+@get_strings_dec("promotes")
+async def promote(message, chat, user, args, strings):
+    chat_id = chat["chat_id"]
+    text = strings["promote_success"].format(
+        user=await get_user_link(user["user_id"]), chat_name=chat["chat_title"]
     )
-    input_str = event.pattern_match.group(1)
-    reply_msg_id = event.message.id
-    if reply_msg_id:
-        r_mesg = await event.get_reply_message()
-        to_promote_id = r_mesg.sender_id
-    elif input_str:
-        to_promote_id = input_str
+
+    if user["user_id"] == BOT_ID:
+        return
+
+    if user["user_id"] == message.from_user.id:
+        return await message.reply(strings["cant_promote_yourself"])
+
+    title = None
+
+    if args:
+        if len(args) > 16:
+            await message.reply(strings["rank_to_loong"])
+            return
+        title = args
+        text += strings["promote_title"].format(role=html.escape(title, quote=False))
+
     try:
-        await event.client(EditAdminRequest(event.chat_id, to_promote_id, rights, ""))
-    except (Exception) as exc:
-        await event.edit(str(exc))
-    else:
-        await event.edit("Successfully Promoted")
+        await tbot.edit_admin(
+            chat_id,
+            user["user_id"],
+            invite_users=True,
+            change_info=True,
+            ban_users=True,
+            delete_messages=True,
+            add_admins=True,
+            pin_messages=True,
+            title=title,
+        )
+    except ValueError:
+        return await message.reply(strings["cant_get_user"])
+    except AdminRankEmojiNotAllowedError:
+        return await message.reply(strings["emoji_not_allowed"])
+    await get_admins_rights(chat_id, force_update=True)  # Reset a cache
+    await message.reply(text)
